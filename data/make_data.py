1407f392a0d748a03536272299888db33ee0ad74
DrownFish19 <DrownFish19@gmail.com>
2024-01-11 23:42:18 +0800

    feat(data):add make data from mysql

--- data/make_data.py
+++ data/make_data.py
@@ -1,168 +1,87 @@
-import os
 
+from datetime import datetime, timedelta
 import pandas as pd
 from mysql_import import connect_mysql
+import  numpy as np
 
-
-def filter_data():
-    chunk_size = 5000000  # 每次读取的行数
-    file_path = "/run/user/1000/gvfs/smb-share:server=ds918.local,share=数据共享/pems.csv"
-
-    date_time_format = "%m/%d/%Y %H:%M:%S"
-    csv_reader = pd.read_csv(
-        file_path,
-        chunksize=chunk_size,
-        parse_dates=["Timestamp"],
-        date_format=date_time_format,
-    )
-
-    # filtered_data = pd.DataFrame()
-
-    start_date = pd.to_datetime("2019-01-01")  # 开始时间
-    end_date = pd.to_datetime("2019-01-31")  # 结束时间
-
-    output_file_prefix = "save/pems_"
-    part_number = 1
-    for chunk in csv_reader:
-        # 筛选字段A和字段B并添加到filtered_data中
-        selected_columns = chunk[
-            [
-                "Timestamp",
-                "ID",
-                "District",
-                "Freeway",
-                "Direction",
-                "LaneType",
-                "StationLength",
-                "numSamples",
-                "percentObs",
-                "TotalFlow",
-                "AvgOccupancy",
-                "AvgSpeed",
-                "LaneNSamples",
-                "LaneNFlow",
-                "LaneNAvgOcc",
-                "LaneNAvgSpeed",
-                "LaneNObserved",
-            ]
-        ]
-        selected_columns = selected_columns[
-            (chunk["Timestamp"] >= start_date) & (chunk["Timestamp"] <= end_date)
-        ]
-        # filtered_data = pd.concat([filtered_data, selected_columns])
-
-        if not selected_columns.empty:
-            # 保存当前块到一个新的文件
-            output_file_path = f"{output_file_prefix}{part_number}.csv"
-            selected_columns.to_csv(output_file_path, index=False)
-
-            part_number += 1
-
-
-def merge_data():
-    # 获取所有以'filtered_data_part_'开头的文件
-    # file_pattern = "save/pems_*.csv"
-    filtered_files = os.listdir("save")
-
-    # 创建一个空的DataFrame来存储合并后的数据
-    merged_data = pd.DataFrame()
-
-    # 遍历所有筛选文件并合并它们
-    for file_path in filtered_files:
-        print(file_path)
-        chunk = pd.read_csv(os.path.join("save", file_path))
-        merged_data = pd.concat([merged_data, chunk], ignore_index=True)
-
-    # 保存合并后的数据到一个新的文件
-    output_merged_file = "merged_filtered_data.csv"
-    merged_data.to_csv(output_merged_file, index=False)
-
-    print(f"Merged data saved to {output_merged_file}")
-
-
-def sort_by_timestamp():
-    input_merged_file = "merged_filtered_data.csv"
-    output_merged_file = "merged_filtered_data_sorted.csv"
-
-    df = pd.read_csv(
-        input_merged_file, parse_dates=["Timestamp"], date_format="%Y-%m-%d %H:%M:%S"
-    )
-    df.sort_values(by=["Timestamp", "ID"], inplace=True)
-
-    # # 指定起始时间和结束时间, 并使用date_range()生成每5分钟的时间序列
-    # start_time = '2019-01-01 00:00:00'
-    # end_time = '2019-02-01 00:00:00'
-    # time_range = pd.date_range(start=start_time, end=end_time, freq='5T')
-    # merged_data = pd.DataFrame()
-    # for idx in range(len(time_range)):
-    #     start_t = time_range[idx]
-    #     end_t = time_range[idx+1]
-    #     print(start_t, end_t, flush=True)
-    #     selected_columns = df[(df['Timestamp'] >= start_t) & (df['Timestamp'] <= end_t)]
-    #     # 按时间戳列进行排序
-    #     selected_columns.sort_values(by=["ID"], inplace=True)
-    #     merged_data = pd.concat([merged_data, selected_columns], ignore_index=True)
-    # # 保存合并后的数据到一个新的文件
-    # merged_data.to_csv(output_merged_file, index=False)
-
-    df.to_csv(output_merged_file, index=False)
-
-
-def data_check():
-    # 读取csv文件
-    df = pd.read_csv(
-        "/root/code/PaddleSTXL/test.csv",
-        parse_dates=["Timestamp"],
-        date_format="%Y-%m-%d %H:%M:%S",
-        index_col="Timestamp",
-    )
-
-    start_time = "2019-01-01 00:00:00"
-    end_time = "2019-01-31 23:55:00"
-    time_range = pd.date_range(start=start_time, end=end_time, freq="5T")
-
-    base_id = df[(df["Timestamp"] == time_range[0])]["ID"]
-    for idx in range(len(time_range)):
-        current_time = time_range[idx]
-        print(current_time, flush=True)
-        selected_columns = df[(df["Timestamp"] == current_time)]
-        if list(selected_columns["ID"]) == list(base_id):
-            print(f"{idx}, {current_time} True")
-        else:
-            print(
-                f"{idx}, {current_time} False, {len(list(selected_columns['ID']))} vs {len(list(base_id))}"
-            )
-
-
-def get_id_intersection(cnx, start_date, end_date, freq="5T"):
-    time_range = pd.date_range(start=start_date, end=end_date, freq=freq)
-    base_sql = "select ID from pems_5min where Timestamp = '{}'"
+# step 1: get id-idx dict
+def get_id_idx_dict(cnx):
+    id_idx_dict = {}
+    sql_str = "SELECT stations_lat_lon.INDEX, stations_lat_lon.ID FROM stations_lat_lon;"
     cursor = cnx.cursor()
+    cursor.execute(sql_str)
+    res = cursor.fetchall()
+    for idx_id in res:
+        id_idx_dict[idx_id[1]] = idx_id[0]
+    return id_idx_dict
 
-    all_ids = []
-    for timestamp in time_range:
-        cur_sql = base_sql.format(timestamp)
-        print(cur_sql)
-        cursor.execute(cur_sql)
-        ids = cursor.fetchall()
-        all_ids.append(ids)
+# step 2: get TotalFlow, AvgSpeed, AvgOccupancy
+def get_traffic_flow_piece(cnx, id_idx_dict, timestamp):
+    n_feat = 1 + 3 # the first feat to flag vaild data
+    traffic_flow_piece = np.zeros([len(id_idx_dict), n_feat])
+    sql_str = f"SELECT ID, TotalFlow, AvgSpeed, AvgOccupancy from pems_5min WHERE Timestamp = '{timestamp}';"
+    cursor = cnx.cursor()
+    cursor.execute(sql_str)
+    res = cursor.fetchall()
+    
+    def data_convert(value):
+        if value is None:
+            return 0
+        else:
+            return value
+            
+            
+    for r in res:
+        if r[0] in id_idx_dict:
+            idx = id_idx_dict[r[0]] - 1
+            traffic_flow_piece[idx, 0] = 1
+            traffic_flow_piece[idx, 1] = data_convert(r[1])
+            traffic_flow_piece[idx, 2] = data_convert(r[2])
+            traffic_flow_piece[idx, 3] = data_convert(r[3]) 
+    return traffic_flow_piece
 
-    if len(all_ids) > 0:
-        # compute intersection
-        intersection = set(all_ids[0])
-        for ids in all_ids[1:]:
-            intersection = intersection & set(ids)
-            print(len(intersection))
-            print(intersection)
+# step 3: get timestamp_idx_dict
+def get_timestamp_idx_dict(cnx):
+    timestamp_idx_dict = {}
+    sql_str = "SELECT `timestamp`.`index`, `timestamp`.`Timestamp` from `timestamp`;;"
+    cursor = cnx.cursor()
+    cursor.execute(sql_str)
+    res = cursor.fetchall()
+    for idx_timestamp in res:
+        timestamp_idx_dict[idx_timestamp[1]] = idx_timestamp[0]
+    return timestamp_idx_dict
 
-        with open("intersection.txt", "w") as f:
-            for id in intersection:
-                f.write(str(id[0]) + "\n")
-    return intersection
 
+# step 4: construct dataset
+def get_traffic_flow_data(cnx, start_date, end_date, freq = "5T"):
+    id_idx_dict = get_id_idx_dict(cnx)
+    timestamps = pd.date_range(start=start_date, end=end_date, freq=freq)
+    timestamp_idx_dict = get_timestamp_idx_dict(cnx)
+    
+    n_feat = 1 + 3 # the first feat to flag vaild data
+    traffic_flow_data = np.zeros([len(timestamps), len(id_idx_dict), n_feat])
+    start_timestamp_idx = timestamp_idx_dict[start_date]-1
+    for timestamp in timestamps:
+        print(timestamp)
+        traffic_flow_piece = get_traffic_flow_piece(cnx, id_idx_dict, timestamp)
+        timestamp_idx = (timestamp_idx_dict[timestamp] - 1) - start_timestamp_idx
+        traffic_flow_data[timestamp_idx] = traffic_flow_piece
+    return traffic_flow_data, id_idx_dict, timestamp_idx_dict
 
 if __name__ == "__main__":
-    cnx = connect_mysql(ip="127.0.0.1", port="3306", username="root", password="wonder")
-    start_date = "2019-01-01 00:00:00"
-    end_date = "2019-01-31 23:55:00"
-    get_id_intersection(None, start_date, end_date, "5T")
+    cnx = connect_mysql(ip="192.168.188.222", port="3306", username="root", password="wonder")
+    
+    input_format = "%Y-%m-%d %H:%M:%S"
+    output_format = "%Y-%m-%d_%H-%M-%S"
+    
+    start_datatime = datetime.strptime("2019-01-24 00:00:00", input_format)
+    end_datetime = datetime.strptime("2019-01-31 23:55:00", input_format)
+    cur_datetime = start_datatime
+    while cur_datetime + timedelta(hours=23) + timedelta(minutes=55) <= end_datetime:
+        s_datetime = cur_datetime
+        e_datetime = cur_datetime + timedelta(hours=23) + timedelta(minutes=55)
+        traffic_flow_data, _, _ = get_traffic_flow_data(cnx, s_datetime, e_datetime, "5T")
+        filename = f"{s_datetime.strftime(output_format)}-{e_datetime.strftime(output_format)}.npy"
+        print("saved", filename)
+        np.save(filename, traffic_flow_data)
+        cur_datetime += timedelta(days=1)
