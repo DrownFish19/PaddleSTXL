af1fb2e03d8f6d18cfb48f96423df97e9631456e
DrownFish19 <DrownFish19@gmail.com>
2024-01-20 14:13:35 +0000

    update last test

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from trainer import Trainer
 if __name__ == "__main__":
     stxl_trainer = Trainer(training_args=args)
     stxl_trainer.train()
-    stxl_trainer.run_test()
+    # stxl_trainer.run_test()
